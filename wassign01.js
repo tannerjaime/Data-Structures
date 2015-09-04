0d5b6a9e60e067d39a46d3ca80a6b44a55ffb936
tannj583 <tannj583@newschool.edu>
2015-09-04 15:42:11 +0000

    Data Structures Week 1

---
+++ wassign01.js
@@ -0,0 +1,11 @@
+var request = require('request');
+var fs = require('fs');
+//making request for the website
+request('http://www.nyintergroup.org/meetinglist/meetinglist.cfm?zone=02&borough=M', function (error, response, body) { //we can expect to possiblt get an error, response, and body, derived from documentation for request
+  if (!error && response.statusCode == 200) {
+    fs.writeFileSync('/home/ubuntu/workspace/data/aameetinglist02M.txt', body); // gunna write it into a text file 
+  }
+  else {console.error('request failed')}
+})
+
+//also had to mkdir data to have a place to send the text file too. 
