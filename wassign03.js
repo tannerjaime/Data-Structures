42061e0014efce0fef3902552d060ac97e6dc2dd
tannj583 <tannj583@newschool.edu>
2015-09-28 19:10:50 +0000

    Weekly Assignment 3

--- wassign03.js
+++ wassign03.js
@@ -5,30 +5,40 @@ var fs = require('fs');
 // SETTING ENVIRONMENT VARIABLES (in Linux): 
 // export NEW_VAR="Content of NEW_VAR variable"
 // printenv | grep NEW_VAR
-var apikey = process.env.API_KEY
+
+var apikey = process.env.API_KEY;
 // var URLtoParse = 
 
 var meetingsData = [];
-var addresses = [fs.readFileSync('/home/ubuntu/workspace/addresses.txt').toString()];
-// console.log(addresses);
+var addresses = [];
+
+//use JSON.parse() to read data into object
+var addresses = fs.readFileSync('/home/ubuntu/workspace/addresses.txt').toString().split(",,");
+console.log(addresses);
+
 
 // eachSeries in the async module iterates over an array and operates on each item in the array in series
 // // calls for array and the function ot ogo over 
+
 async.eachSeries(addresses, function(value, callback) {
     var apiRequest = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + value.split(' ').join('+') + '&key=' + apikey;
     var thisMeeting = new Object;
     thisMeeting.address = value;
+
+    // var apiRequest = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + cleanAddress + '&key=' + apikey;
+    // console.log(cleanAddress[2]);
+    console.log(apiRequest);
     request(apiRequest, function(err, resp, body) {
-        if (err) {throw err;}
+        if (err) {
+            throw err;
+        }
+
         thisMeeting.latLong = JSON.parse(body).results[0].geometry.location; //.parse indicates that its an object 
         meetingsData.push(thisMeeting);
+
     });
-    setTimeout(callback, 4000);
+    setTimeout(callback, 500);
 }, function() {
-    console.log(meetingsData);
+    // console.log(meetingsData);
+    fs.writeFileSync('./aaMeetingsArray.txt', JSON.stringify(meetingsData));
 });
-
-//how to break the code down so that its not to many requests
-
-//save array 2 as text file. start new program that reads in that text file and goes down that path. 
-
