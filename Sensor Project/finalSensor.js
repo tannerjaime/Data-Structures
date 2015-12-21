8e5a2af9bf7c630959fb258953e67fedb774c4c2
tannerjaime <tannj583@gmail.com>
2015-12-21 14:39:36 -0500

    :thumbsup:

---
+++ Sensor Project/finalSensor.js
@@ -0,0 +1,48 @@
+var http = require('http');
+var pg = require('pg');
+
+// supply connection string through an environment variable
+var conString = "postgres://jaime:parsonsdv@data-structures.cdp3q92nnmui.us-west-2.rds.amazonaws.com:5432/postgres";
+
+
+var server = http.createServer(function(req, res) {
+
+    // get a pg client from the connection pool
+    pg.connect(conString, function(err, client, done) {
+
+        var handleError = function(err) {
+            // no error occurred, continue with the request
+            if (!err) return false;
+
+            // An error occurred, remove the client from the connection pool.
+            // A truthy value passed to done will remove the connection from the pool
+            // instead of simply returning it to be reused.
+            // In this case, if we have successfully received a client (truthy)
+            // then it will be removed from the pool.
+            if (client) {
+                done(client);
+            }
+            res.writeHead(500, {'content-type': 'text/plain'});
+            res.end('An error occurred');
+            return true;
+        };
+
+        // handle an error from the connection
+        if (handleError(err)) return;
+
+        // get the total number of visits today (including the current visit)
+        client.query('SELECT * FROM gender ORDER BY timedate DESC LIMIT 16;', function(err, result) {
+            console.log(result.rows);
+            // handle an error from the query
+            if (handleError(err)) return;
+            
+            // return the client to the connection pool for other requests to reuse
+            done();
+            res.writeHead(200, {'content-type': 'application/json'});
+            res.write(JSON.stringify(result.rows));
+            res.end();
+        });
+    });
+});
+
+server.listen(process.env.PORT);
